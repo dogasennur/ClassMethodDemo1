a589a2f88db2a9574c5c0796c16837e6c9bb79cc
Lenovo <Lenovo@DOAEN>
2023-02-15 13:49:35 +0300

    Add project files.

---
+++ ClassMethodDemo1/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo1
+{
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine("Bankamıza hoşgeldiniz"+ "="+ musteri.firstName);
+        
+        }   
+        public void Listele(Musteri musteri) 
+        {
+            Console.WriteLine("müşteriler listelendi "+ "="+ musteri.firstName);
+        
+        }
+
+        public void Silme(Musteri musteri) 
+        {
+            Console.WriteLine("hoşçakalın"+ "="+ musteri.firstName);
+        
+        
+        }
+
+        public void Add(string firstName, int id) 
+        {
+            Console.WriteLine(" bankamıza hoşgeldiniz: "+ firstName , id);
+        }
+    }
+}
---
+++ ClassMethodDemo1/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMethodDemo1;
+
+Console.WriteLine("Hello, World!");
+
+
+Musteri musteri1=new Musteri();
+musteri1.id = 123;
+musteri1.firstName = "merve";
+musteri1.lastName = "yıldız";
+musteri1.age = 23;
+
+Musteri musteri2=new Musteri();
+musteri2.id = 456;
+musteri2.firstName = "doğa";
+musteri2.lastName = "gök";
+musteri2.age = 24;
+
+Musteri musteri3=new Musteri();
+musteri3.id = 789;
+musteri3.firstName = "barış";
+musteri3.lastName = "güneş";
+musteri3.age = 22;
+
+Musteri[] musteriler= new Musteri[] { musteri1, musteri2, musteri3 };
+
+foreach (Musteri musteri in musteriler)
+
+{
+    Console.WriteLine(musteri.id);
+    Console.WriteLine(musteri.firstName);
+    Console.WriteLine(musteri.lastName);
+    Console.WriteLine(musteri.age);
+    Console.WriteLine("--------------");
+}
+
+MusteriManager musterimanager= new MusteriManager();
+musterimanager.Ekle(musteri1);
+musterimanager.Ekle(musteri2);
+musterimanager.Ekle(musteri3);
+
+musterimanager.Listele(musteri1);
+musterimanager.Listele(musteri2);
+musterimanager.Listele(musteri3);
+
+musterimanager.Silme(musteri1);
+musterimanager.Silme(musteri2);
+musterimanager.Silme(musteri3);
+
+
+
+
+musterimanager.Add("doğa", 456);
